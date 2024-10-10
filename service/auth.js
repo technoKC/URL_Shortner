5c372705e18a159c9855a7a8342edb31ee0175d2
@kc <kcbxr2345@gmail.com>
2024-10-10 11:21:15 +0530

    adding

---
+++ service/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const secret = "khushi06";
+
+function setUser(user){
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    },
+    secret
+);
+}
+function getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token,secret);
+    } catch (error) {
+        return null;
+    }
+}
+module.exports ={
+    setUser,
+    getUser,
+};
